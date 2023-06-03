baa2d669eda1a99d439e4b88c7bed44bfc1c1334
Troy Fullwood <everyfan3417@gmail.com>
2023-06-03 16:22:42 -0400

    Make Everything Typed Fixes popeyeotaku/ttyemu#1

--- slowpty.py
+++ slowpty.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-"Simple script to reduce output speed."
-# Doing it inside the terminal emulator alone causes serious problems for
-# non-pty backends. This allows e.g. interrupting a long output to work more
-# like how it would have on classic systems. Works in WSL and Mac, but not
-# Linux. There seems to be no way to get nice behavior at all on Linux.
+"""Simple script to reduce output speed.
+
+Doing it inside the terminal emulator alone causes serious problems for
+non-pty backends. This allows e.g. interrupting a long output to work more
+like how it would have on classic systems. Works in WSL and Mac, but not
+Linux. There seems to be no way to get nice behavior at all on Linux.
+"""
 
 import pty
 import os
@@ -15,12 +17,10 @@ import tty
 
 # pylint: disable=invalid-name,protected-access,broad-except
 def main():
-    "Main function"
+    """Run the main function."""
     attr = termios.tcgetattr(0)
     pid, fd = pty.fork()
-    cmd = sys.argv[1:] or ['sh']
-    CHARS_PER_SEC = 10
-    CHAR_DELAY = 1/CHARS_PER_SEC
+    cmd = sys.argv[1:] or ["sh"]
     if pid == 0:
         try:
             attr[4] = attr[5] = termios.B110
@@ -44,5 +44,6 @@ def main():
                 time.sleep(0.1)
     finally:
         termios.tcsetattr(0, termios.TCSAFLUSH, attr)
+
 
 main()
